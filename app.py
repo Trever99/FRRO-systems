4980d304fe7c89a59844394b34d72fea55e7d89b
Trever99 <skmariro@gmail.com>
2026-03-21 08:20:58 +0530

    collecting Data Ftom Html from nd inserting into DB

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import os, sqlite3
 #DB imports
 from config import Config
-from models import db
+from models import db, Student, Document
 
 app = Flask(__name__)
 # Database configuration
@@ -25,11 +25,11 @@ if not os.path.exists(UPLOAD_FOLDER):
 @app.route("/")
 def home():
     return render_template("index.html")
-    print("Db created successfully!")
+    
 
 # Show Add Student Form
 @app.route("/add")
-def add_student():
+def add_student(): 
     return render_template("add_student.html")
 
 #Return to Dashboard
@@ -50,19 +50,47 @@ def search():
     return render_template("index.html", search_query=query, message=message)
 
 # Handle Form Submission
-@app.route("/submit", methods=["POST"])
+@app.route("/submit", methods=["GET", "POST"])
 def submit_student():
-    data = request.form
+     data = request.form
 
-    file = request.files['document']
-    filename = file.filename
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+     file = request.files['document']
+     filename = file.filename
+     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+     if request.method == 'POST':
+        
+        #Get Data From HTML Form
+        name = request.form['name']
+        surname = "Default"
+        roll_no = request.form['roll']
+        phone_nr = request.form['phone']
+        email = request.form['email']
+        
+        country_name = request.form['country']
+        course_name = request.form['course']
+        
+        #Create the student object
+        student = Student(
+            roll_no=roll_no,
+            name=name,
+            surname=surname,
+            phone_nr=phone_nr,
+            email=email,
+            course_id=1,  # Placeholder, should be set based on course_name
+            country_id=1  # Placeholder, should be set based on country_name
+        )
+        #Add to DB
+        db.session.add(student)
+        db.session.commit()
+        
+        return redirect(url_for('add_student'))
 
     # For now, it will just print the data (we will connect DB later)
-    print("Student Data:", data)
-    print("File uploaded:", filename)
+    # print("Student Data:", data)
+    # print("File uploaded:", filename)
 
-    return "Student added successfully! Please return to the dashboard."
+     return "Student added successfully! Please return to the dashboard."
 
 #Base models
 
