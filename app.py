3414a71167678b7e45502b678d2faac0549f68a2
Trever99 <skmariro@gmail.com>
2026-03-23 15:15:27 +0530

    Added validation and unickue check

--- app.py
+++ app.py
@@ -52,7 +52,11 @@ def search():
 # Handle Form Submission
 @app.route("/submit", methods=["GET", "POST"])
 def submit_student():
-     data = request.form
+    ##Thread issue solution with conn
+    
+     conn = sqlite3.connect(r"C:\Users\EazyPC\OneDrive\Documents\A.PROJECTS\FRRO managment system\FRRO-systems\students.db")
+     cursor = conn.cursor()
+    #  data = request.form
 
      file = request.files['document']
      filename = file.filename
@@ -62,16 +66,31 @@ def submit_student():
         
         #Get Data From HTML Form
         name = request.form['name']
-        surname = "Default"
+        surname = "..."
         roll_no = request.form['roll']
         phone_nr = request.form['phone']
         email = request.form['email']
-        
         country_name = request.form['country']
         course_name = request.form['course']
         
+        if len(name) < 3:
+            message = "enter valid name"
+            return render_template("add_student.html", message=message)
+        elif len(phone_nr) != 10:
+            message = "enter valid phone number"
+            return render_template("add_student.html", message=message)
+        elif len(email) < 5 or "@" not in email:
+            message = "enter valid email"
+            return render_template("add_student.html", message=message)
+        elif len(roll_no) < 7:
+            message = "enter valid roll number(at least 7 characters)"
+            return render_template("add_student.html", message=message)
+        else:
+        
+       
+        
         #Create the student object
-        student = Student(
+          student = Student(
             roll_no=roll_no,
             name=name,
             surname=surname,
@@ -79,18 +98,39 @@ def submit_student():
             email=email,
             course_id=1,  # Placeholder, should be set based on course_name
             country_id=1  # Placeholder, should be set based on country_name
-        )
-        #Add to DB
-        db.session.add(student)
-        db.session.commit()
+           )
+        #validation and error handling can be added here (e.g., check for empty fields, validate email format, etc.)
+          existing = Student.query.filter(
+            (Student.email == email) | 
+            (Student.roll_no == roll_no) | 
+            (Student.phone_nr == phone_nr)
+          )
         
-        return redirect(url_for('add_student'))
-
-    # For now, it will just print the data (we will connect DB later)
-    # print("Student Data:", data)
-    # print("File uploaded:", filename)
-
-     return "Student added successfully! Please return to the dashboard."
+        ## Checking if the fields exists
+          existing_email = any(s.email == email for s in existing)
+          existing_roll = any(s.roll_no == roll_no for s in existing)
+          existing_phone = any(s.phone_nr == phone_nr for s in existing)   
+        
+        # If any of the fields already exists, show an error message
+      
+          if existing_email:
+            message = "A student with the same email already exists."
+            return render_template("add_student.html", message=message)
+          elif existing_roll:
+            message = "A student with the same roll number already exists."
+            return render_template("add_student.html", message=message)
+          elif existing_phone:
+            message = "A student with the same phone number already exists."
+            return render_template("add_student.html", message=message)
+    
+          else:
+        # Add student to the database
+           db.session.add(student)
+           db.session.commit()
+           return render_template("add_student.html", message="Student added successfully! Please return to the dashboard.")
+          return redirect(url_for('add_student'))
+        
+     
 
 #Base models
 
--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-import os
+import os , sqlite3
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+conn = sqlite3.connect(os.path.join(BASE_DIR, 'students.db'))
+cursor = conn.cursor()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'students.db')
