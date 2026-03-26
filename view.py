0bffde725bdb99d3d5c796f34d97f0da55d97800
Andrewmatewere1 <andrewmatewere1@gmail.com>
2026-03-26 14:21:09 +0530

    update view,edit,delete pages

--- view.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from models import Student , db
-
-with app.app_context():
-    students = Student.query.all()
-    for s in students:
-        print(s.roll_no, s.name, s.surname, s.phone_nr, s.email, s.course_id, s.country_id)
-        
-        
